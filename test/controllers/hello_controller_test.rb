d20c4086993f9297b85a15557cf0c370e6bcdbdf
MacUser <sou.8216028k@ozzio.jp>
2018-07-18 23:11:08 +0900

    8章終了

--- test/controllers/hello_controller_test.rb
+++ test/controllers/hello_controller_test.rb
@@ -4,4 +4,19 @@ class HelloControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  # test 'list action' do
+  #     get '/hello/list'
+  #     assert_equal 10, assigns(:books).length, 'found rows is wrong.'
+  #     assert_response :success, 'list action failed.'
+  #     assert_template 'hello/list'
+  # end
+
+  test "routing check" do
+      assert_generates('hello/list', { controller: 'hello', action: 'list' })
+  end
+
+  test "routing check" do
+      
+  end
 end
--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,40 @@
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
+    def setup
+        @b = Books(:modernjs)
+    end
+
+    def teardown
+        @b = nil
+    end
+
+    test "book save" do
+        book = Book.new({
+            isbn: '978-4-7741-4466-X',
+            title: 'Ruby on Rails本格入門',
+            price: 3100,
+            publish: '技術評論社',
+            published: '2017-02-14',
+            dl: false
+            })
+        assert !book.save, 'Failed to save'
+        assert_equal book.errors.size 2, 'Failed to validate count'
+        assert book.errors[:isbn].any?, 'Failed to isbn validate'
+    end
+
+    test "where method test" do
+        result = Book.find_by(title: '改訂新版JavaScript本格入門')
+        assert_instance_of Book, result, 'result is not instance of Book'
+        assert_equal books(:modernjs).isbn, result.isbn, 'isbn column is wrong.'
+        assert_equal Date.new(2016, 9, 30), result.published, 'published colunm is wrong.'
+    end
   # test "the truth" do
   #   assert true
   # end
+
+  test "where method test" do
+      assert_equal @b.isbn, result.isbn, 'isbn column is wrong.'
+      assert_equal @b.published, 'published column is wrong'
+  end
 end
