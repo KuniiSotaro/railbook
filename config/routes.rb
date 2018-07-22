f0f328ce177e76ce3a12df9369a004b23c27a162
MacUser <sou.8216028k@ozzio.jp>
2018-07-22 23:14:52 +0900

    9章強制終了

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,8 @@ Rails.application.routes.draw do
   get 'hello/index', to: 'hello#index'
   get 'hello/view'
   get 'hello/list'
+  get 'ajax/upanel', to: 'ajax#upanel'
+  # post 'ajax/upanel', to: 'ajax#upanel'
+  get 'ajax/search', to: 'ajax#search'
+  post 'ajax/result', to: 'ajax#result'
 end
